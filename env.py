96202080cb6529ded3f2417e88c801081ad1dce7
jenns93 <jjenns93@gmail.com>
2022-03-14 14:17:48 +0000

    deployment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://hlqzhtxosowbvz:fc965ef59c820b47524c9812e3c36e283332bc3decaf3342d262fdf77fb42cb8@ec2-63-34-153-52.eu-west-1.compute.amazonaws.com:5432/d4j4ak5v8qaobm"
+os.environ["SECRET_KEY"]="secret_key1085"
+os.environ["CLOUDINARY_URL"]="cloudinary://271659233865527:bFO42VjRSAD5Rc2J3Utv6NtHpgo@dfa0augxk"
